0fe3513ea81815c575253f00d5e1fd6f34ee2503
Thomas <thomasg@techaffinity.com>
2023-01-31 17:24:12 +0200

    login page update

---
+++ sites/default/files/php/twig/63d9305b2309e_page--user--login.html.tw_iNRhkrcmHBzCpvWfY2mAi4BnY/k1d4IxxfEpR5DE3oLt_JY6O9foNkVXqC3k8jJ0nXDwc.php
@@ -0,0 +1,275 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/model/templates/pages/page--user--login.html.twig */
+class __TwigTemplate_da3ddc93c1d17b1d8f76905f9c8e5fd26519273f28ad0875adc14622cc90ea2d extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <title>User Login | RFMU</title>
+    <!-- plugins:css -->
+    <link rel=\"stylesheet\" href=\"";
+        // line 9
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 9, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/admin-assets/assets/vendors/mdi/css/materialdesignicons.min.css\">
+    <link rel=\"stylesheet\" href=\"";
+        // line 10
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 10, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/admin-assets/assets/vendors/css/vendor.bundle.base.css\">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel=\"stylesheet\" href=\"";
+        // line 17
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 17, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/admin-assets/assets/css/style.css\">
+    <!-- End layout styles -->
+  </head>
+  <body>
+    <div class=\"container-scroller\">
+      <div class=\"container-fluid page-body-wrapper full-page-wrapper\">
+        <div class=\"content-wrapper d-flex align-items-center auth\" style=\"padding: 2.75rem 1.25rem;\">
+          <div class=\"row flex-grow\">
+            <div class=\"col-lg-6 mx-auto\">
+              <div class=\"auth-form-light text-left p-5\">
+                <div class=\"brand-logo text-center\">
+                  <img src=\"";
+        // line 28
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 28, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/assets/model.jpg\" alt=\"Rwanda Fashion Model Union\" style=\"max-width: 18%;font-size: medium !important;margin:auto;\">
+                </div>
+                <!-- <h4>Hello! let's get started</h4> -->
+                <h6 class=\"font-weight-light\">Sign in to continue.</h6>
+                <div class=\"pt-3\">
+                  ";
+        // line 33
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "message", [], "any", false, false, true, 33), 33, $this->source), "html", null, true);
+        echo "
+                  ";
+        // line 34
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "content", [], "any", false, false, true, 34), 34, $this->source), "html", null, true);
+        echo "
+
+                  <div class=\"mt-3\">
+                   <a href=\"";
+        // line 37
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 37, $this->source), "html", null, true);
+        echo "user/password\" class=\"auth-link text-black\">Forgot password?</a>
+                  </div>
+                  <div class=\"mt-3\">
+                    <a class=\"btn btn-block btn-info form-control btn-lg font-weight-medium auth-form-btn\" href=\"";
+        // line 40
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 40, $this->source), "html", null, true);
+        echo "register-model\">Create Model Account</a>
+                  </div>
+                  <div class=\"mt-3\">
+                    <a class=\"btn btn-block btn-info form-control btn-lg font-weight-medium auth-form-btn\" href=\"";
+        // line 43
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 43, $this->source), "html", null, true);
+        echo "register-agency\">Create Agency Account</a>
+                  </div>
+                  <div class=\"mt-3\">
+                    <a class=\"btn btn-block btn-warning form-control btn-lg font-weight-medium auth-form-btn\" href=\"";
+        // line 46
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 46, $this->source), "html", null, true);
+        echo "\">Go To Home</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src=\"";
+        // line 59
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 59, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/admin-assets/assets/vendors/js/vendor.bundle.base.js\"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src=\"";
+        // line 64
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 64, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/admin-assets/assets/js/off-canvas.js\"></script>
+    <script src=\"";
+        // line 65
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 65, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/admin-assets/assets/js/hoverable-collapse.js\"></script>
+    <script src=\"";
+        // line 66
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["base_path"] ?? null), 66, $this->source), "html", null, true);
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->getActiveThemePath(), "html", null, true);
+        echo "/admin-assets/assets/js/misc.js\"></script>
+    <!-- endinject -->
+  </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/model/templates/pages/page--user--login.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  152 => 66,  147 => 65,  142 => 64,  133 => 59,  117 => 46,  111 => 43,  105 => 40,  99 => 37,  93 => 34,  89 => 33,  80 => 28,  65 => 17,  54 => 10,  49 => 9,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <title>User Login | RFMU</title>
+    <!-- plugins:css -->
+    <link rel=\"stylesheet\" href=\"{{base_path}}{{ active_theme_path() }}/admin-assets/assets/vendors/mdi/css/materialdesignicons.min.css\">
+    <link rel=\"stylesheet\" href=\"{{base_path}}{{ active_theme_path() }}/admin-assets/assets/vendors/css/vendor.bundle.base.css\">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel=\"stylesheet\" href=\"{{base_path}}{{ active_theme_path() }}/admin-assets/assets/css/style.css\">
+    <!-- End layout styles -->
+  </head>
+  <body>
+    <div class=\"container-scroller\">
+      <div class=\"container-fluid page-body-wrapper full-page-wrapper\">
+        <div class=\"content-wrapper d-flex align-items-center auth\" style=\"padding: 2.75rem 1.25rem;\">
+          <div class=\"row flex-grow\">
+            <div class=\"col-lg-6 mx-auto\">
+              <div class=\"auth-form-light text-left p-5\">
+                <div class=\"brand-logo text-center\">
+                  <img src=\"{{base_path}}{{ active_theme_path() }}/assets/model.jpg\" alt=\"Rwanda Fashion Model Union\" style=\"max-width: 18%;font-size: medium !important;margin:auto;\">
+                </div>
+                <!-- <h4>Hello! let's get started</h4> -->
+                <h6 class=\"font-weight-light\">Sign in to continue.</h6>
+                <div class=\"pt-3\">
+                  {{page.message}}
+                  {{page.content}}
+
+                  <div class=\"mt-3\">
+                   <a href=\"{{base_path}}user/password\" class=\"auth-link text-black\">Forgot password?</a>
+                  </div>
+                  <div class=\"mt-3\">
+                    <a class=\"btn btn-block btn-info form-control btn-lg font-weight-medium auth-form-btn\" href=\"{{base_path}}register-model\">Create Model Account</a>
+                  </div>
+                  <div class=\"mt-3\">
+                    <a class=\"btn btn-block btn-info form-control btn-lg font-weight-medium auth-form-btn\" href=\"{{base_path}}register-agency\">Create Agency Account</a>
+                  </div>
+                  <div class=\"mt-3\">
+                    <a class=\"btn btn-block btn-warning form-control btn-lg font-weight-medium auth-form-btn\" href=\"{{base_path}}\">Go To Home</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src=\"{{base_path}}{{ active_theme_path() }}/admin-assets/assets/vendors/js/vendor.bundle.base.js\"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src=\"{{base_path}}{{ active_theme_path() }}/admin-assets/assets/js/off-canvas.js\"></script>
+    <script src=\"{{base_path}}{{ active_theme_path() }}/admin-assets/assets/js/hoverable-collapse.js\"></script>
+    <script src=\"{{base_path}}{{ active_theme_path() }}/admin-assets/assets/js/misc.js\"></script>
+    <!-- endinject -->
+  </body>
+</html>", "themes/model/templates/pages/page--user--login.html.twig", "/Applications/XAMPP/xamppfiles/htdocs/rfmu/themes/model/templates/pages/page--user--login.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 9);
+        static $functions = array("active_theme_path" => 9);
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                ['active_theme_path']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
